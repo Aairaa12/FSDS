d03ced8e1f0e989311e1593d49746ba85d2515dc
Aairaa12 <95518033+Aairaa12@users.noreply.github.com>
2022-03-13 22:25:17 +0530

    Add files via upload

---
+++ my_module.py
@@ -0,0 +1,7 @@
+data = {"Name" : "iNeuron", "Course" : ["datascience", "blockchain","drone","robotics","cloud"], "greeting" : "greeting from iNeuron"}
+
+def get_course():
+	return data["Course"]
+
+def get_greeting():
+	return data["greeting"]
